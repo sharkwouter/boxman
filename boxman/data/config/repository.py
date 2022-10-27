c3ead29357650d75c57af3dd76ad0aa15b8c7747
Wouter Wijsman <wouter@gridscale.io>
2022-10-27 15:21:26 +0200

    Use urljoin to form database file url

--- boxman/data/config/repository.py
+++ boxman/data/config/repository.py
@@ -1,5 +1,6 @@
 import os.path
 from dataclasses import dataclass
+from urllib.parse import urljoin
 
 
 @dataclass
@@ -9,7 +10,7 @@ class Repository:
 
     @property
     def database_url(self) -> str:
-        return f"{self.server}/{self.name}.db"
+        return urljoin(f"{self.server}/", f"{self.name}.db")
 
     def get_database_path(self, db_path: str) -> str:
         return os.path.join(db_path, f"{self.name}.db")
